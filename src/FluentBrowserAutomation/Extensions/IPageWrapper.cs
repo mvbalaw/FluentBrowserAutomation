8f169100009e691fddf827f6330352938da2a9cd
Clinton <csheppard@mvbalaw.com>
2013-05-10 14:02:56 -0500

    migrate IPageWrapper and UiState to this project

---
+++ src/FluentBrowserAutomation/Extensions/IPageWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//// ReSharper disable CheckNamespace
+namespace FluentBrowserAutomation
+//// ReSharper restore CheckNamespace
+{
+	public interface IPageWrapper
+	{
+		IBrowserContext BrowserContext { get; }
+	}
+
+	public static class IPageWrapperExtensions
+	{
+		public static T UiState<T>(this T pageWrapper, params Action<IBrowserContext>[] funcs) where T : IPageWrapper
+		{
+			foreach (var func in funcs)
+			{
+				func(pageWrapper.BrowserContext);
+			}
+			return pageWrapper;
+		}
+	}
+}
