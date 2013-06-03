58720c1f32479eca3c7558444eb9ad2eae554083
Weston M. Binford III <wbinford@gmail.com>
2013-06-02 20:44:53 -0500

    Standarized the build process, updated README.md
    Standardized the build process to match the current process used by most
    Mvba projects. Added a License.txt file and associated *.bat files to
    assist in building.
    
    Added CommonAssemblyInfo.cs to manage versioning.
    
    Cleaned up the README.md file and added a link to the Mvba github page.
    
    Updated to following support dlls:
    
    FluentAssert to version 1.0.0.3
    JetBrains.Annotations to version 8.0.4.1195

--- src/FluentBrowserAutomation/Properties/AssemblyInfo.cs
+++ src/FluentBrowserAutomation/Properties/AssemblyInfo.cs
@@ -6,13 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly:AssemblyTitle("FluentBrowserAutomation - http://github.com/mvbalaw/FluentBrowserAutomation/ - MIT License")]
-[assembly:AssemblyDescription("")]
-[assembly:AssemblyConfiguration("")]
-[assembly:AssemblyCompany("McCreary, Veselka, Bragg & Allen, P.C.")]
-[assembly:AssemblyProduct("FluentBrowserAutomation")]
-[assembly: AssemblyCopyright("Copyright © McCreary, Veselka, Bragg & Allen, P.C. 2010-2013")]
-[assembly:AssemblyTrademark("")]
-[assembly:AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,17 +16,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("f12ec700-8203-5320-0000-427034710c00")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly:AssemblyVersion("1.0.*")]
-//[assembly:AssemblyFileVersion("1.0.0.0")]
