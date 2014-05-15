a13f31b2d169cf310842523631d34c7d5bf73a8d
Shashank Shetty <sshetty@mvbalaw.com>
2014-05-15 11:27:52 -0500

    added close tab and switch control to new tab method

--- src/FluentBrowserAutomation/Extensions/IPageWrapper.cs
+++ src/FluentBrowserAutomation/Extensions/IPageWrapper.cs
@@ -1,33 +1,54 @@
 ﻿using System;
+using OpenQA.Selenium;
 
 // ReSharper disable once CheckNamespace
+
 namespace FluentBrowserAutomation
 {
-	public interface IPageWrapper
-	{
-		IBrowserContext BrowserContext { get; }
-	}
+    public interface IPageWrapper
+    {
+        IBrowserContext BrowserContext { get; }
+    }
 
-	public static class IPageWrapperExtensions
-	{
-		public static T UiState<T>(this T pageWrapper, params Action<IBrowserContext>[] funcs) where T : IPageWrapper
-		{
-			return UiState(pageWrapper, null, funcs);
-		}
+    public static class IPageWrapperExtensions
+    {
+        public static T UiState<T>(this T pageWrapper, params Action<IBrowserContext>[] funcs) where T : IPageWrapper
+        {
+            return UiState(pageWrapper, null, funcs);
+        }
 
-		public static T UiState<T>(this T pageWrapper, string activityDescription = null, params Action<IBrowserContext>[] funcs) where T : IPageWrapper
-		{
-			for (var index = 0; index < funcs.Length; index++)
-			{
-				var func = funcs[index];
-				var func1 = func;
-				pageWrapper.BrowserContext.WaitUntil(x =>
-				{
-					func1(pageWrapper.BrowserContext);
-					return true;
-				}, errorMessage: "UiState action " + (index + 1) + " of " + (activityDescription??funcs.Length.ToString()) + " failed.");
-			}
-			return pageWrapper;
-		}
-	}
+        public static T UiState<T>(this T pageWrapper, string activityDescription = null,
+            params Action<IBrowserContext>[] funcs) where T : IPageWrapper
+        {
+            for (int index = 0; index < funcs.Length; index++)
+            {
+                var func = funcs[index];
+                var func1 = func;
+                pageWrapper.BrowserContext.WaitUntil(x =>
+                {
+                    func1(pageWrapper.BrowserContext);
+                    return true;
+                },
+                    errorMessage:
+                        "UiState action " + (index + 1) + " of " + (activityDescription ?? funcs.Length.ToString()) +
+                        " failed.");
+            }
+            return pageWrapper;
+        }
+
+        public static T SwitchToNewTabAndCloseCurrentTab<T>(this T pageWrapper, Func<IWebDriver, bool> getTabIdentifier) where T : IPageWrapper
+        {
+            var browser = pageWrapper.BrowserContext.Browser;
+            browser.Close();
+            foreach (string handle in browser.WindowHandles)
+            {
+                browser.SwitchTo().Window(handle);
+                if (getTabIdentifier(browser))
+                {
+                    return pageWrapper;
+                }
+            }
+            return pageWrapper;
+        }
+    }
 }
